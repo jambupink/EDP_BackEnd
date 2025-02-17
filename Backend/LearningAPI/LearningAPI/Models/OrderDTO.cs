d3e9dd79e4140a8132bb1620b1789a65ad1008a7
brannnnnnnd <brandonfrancisco512@gmail.com>
2025-02-17 22:04:45 +0800

    Admin view all orders

---
+++ Backend/LearningAPI/LearningAPI/Models/OrderDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace LearningAPI.Models
+{
+	public class OrderDTO
+	{
+		public int OrderId { get; set; }
+		public DateTime OrderDate { get; set; }
+		public string OrderStatus { get; set; } = "Pending";
+		public List<OrderItem> OrderItems { get; set; }
+		public DateTime? DeliveryDate { get; set; }
+	}
+
+}
